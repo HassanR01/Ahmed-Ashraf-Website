54b07d0006ca7be968d5d1b46908d507638862e2
hassanR01 <hassanrageh.236@gmail.com>
2023-12-27 00:27:36 +0200

    Finish without last work

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,20 @@
 let menu = document.querySelector('.menu')
 let icon = document.querySelector('.bIcon')
-
-icon.addEventListener('click' , () => {
+let spanIcons = document.querySelectorAll('.bIcon span')
+icon.addEventListener('mouseover' , () => {
     menu.classList.toggle('active')
+    spanIcons.forEach(spanIcon => {
+        spanIcon.classList.toggle("full")
+    })
 })
+
+icon.addEventListener('mouseout' , () => {
+    menu.classList.toggle('active')
+    spanIcons.forEach(spanIcon => {
+        spanIcon.classList.toggle("full")
+    })
+})
+
+let date = document.querySelector('.dateCR')
+let year = new Date().getFullYear()
+date.textContent = `${year}`
